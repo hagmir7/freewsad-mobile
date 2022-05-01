240f10d82a1bad2fcdf36e7a770b5e6286faffd6
Hassan Agmir <hagmir6@gmail.com>
2022-05-01 12:24:31 +0000

    ads

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,10 @@
-import GoogleAd from "../ads/Hr";
+import GoogleAd from "../GoogleAd/Hr";
 
 
 const About = ()=>{
     return(
         <div>
-            
+            <GoogleAd slot="4567237334" googleAdId="pub-6043226569102012"/>
         </div>
     )
 }
